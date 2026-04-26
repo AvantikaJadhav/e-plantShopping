31f50b33a0ea244ee5fcec29ef2de9197c129c4c
Avantika Jadhav <avantika.jadhav23@vit.edu>
2026-04-26 14:07:37 +0530

    fixed App.jsx for Get Started functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
 import ProductList from "./components/ProductList";
 import CartItem from "./components/CartItem";
-import "./App.css";
+import AboutUs from "./components/AboutUs";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [showApp, setShowApp] = useState(false);
+
   return (
     <BrowserRouter>
-      <div className="landing">
-        <h1>Paradise Nursery 🌱</h1>
-        <Link to="/plants">
-          <button>Get Started</button>
-        </Link>
-      </div>
-      <Link to="/cart">
-  <button>Go to Cart</button>
-</Link>
 
-      <Routes>
-        <Route path="/plants" element={<ProductList />} />
-        <Route path="/cart" element={<CartItem />} />
-      </Routes>
+      {/* LANDING PAGE */}
+      {!showApp ? (
+        <div className="landing">
+          <h1>e-plantShopping 🌱</h1>
+
+          <button onClick={() => setShowApp(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <>
+          {/* NAVBAR */}
+          <nav className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/plants">Plants</Link>
+            <Link to="/cart">Cart</Link>
+            <Link to="/about">About</Link>
+          </nav>
+
+          {/* ROUTES */}
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/plants" element={<ProductList />} />
+            <Route path="/cart" element={<CartItem />} />
+            <Route path="/about" element={<AboutUs />} />
+          </Routes>
+        </>
+      )}
+
     </BrowserRouter>
   );
 }
