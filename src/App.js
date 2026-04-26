a0b2355213311b7aa6acb34c91952c4e4acfc925
Avantika Jadhav <avantika.jadhav23@vit.edu>
2026-04-26 10:58:29 +0530

    Added AboutUs component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AboutUs from "./components/AboutUs";
+
+function App() {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        We provide beautiful indoor plants to make your home fresh and green.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
